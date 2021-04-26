06a92a2e1c2dceebcdc0cf88c1e355f24aae4399
Constantine Nguyen <constantine@ncs.com.sg>
2021-04-26 11:56:48 +0800

    26/04/2021 11:56

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(loadedDeck))
+	}
+
+	if loadedDeck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", loadedDeck[0])
+	}
+
+	if loadedDeck[len(loadedDeck)-1] != "Four of Clubs" {
+		t.Errorf("Last card of Four of Clubs, but got %v", loadedDeck[len(loadedDeck)-1])
+	}
+
+	os.Remove("_decktesting")
+}
