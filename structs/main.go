3862d65291229007af7f82d4478df01a357da83c
joconstantine <joconstantine@users.noreply.github.com>
2021-04-28 18:11:58 +0800

    28/04/2021 18:11

--- structs/main.go
+++ structs/main.go
@@ -27,8 +27,8 @@ func main() {
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
