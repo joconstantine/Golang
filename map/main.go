9fab0074330bd19ea568c1a77758cd2590ff3c3d
Constantine Nguyen <constantine@ncs.com.sg>
2021-04-29 18:05:44 +0800

    29/04/2021 18:05

--- map/main.go
+++ map/main.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
 
-	colors := make(map[int]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-	colors[10] = "#ffffff"
-	delete(colors, 10)
+	printMap(colors)
+}
 
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hext code for", color, "is", hex)
+	}
 }
